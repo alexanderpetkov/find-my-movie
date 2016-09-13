0e2457732149058ab42feec89edf4bdb1ffb041b
Alexander Petkov <milchev.alexander@gmail.com>
2016-09-13 19:27:49 +0300

    Enable autocomplete for movies. TODO: Add to front

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -10,16 +10,29 @@ class Movie < ApplicationRecord
   has_many :genre_belongings
 
   settings index: { number_of_shards: 1 } do
-    mappings dynamic: 'false' do
+    mappings dynamic: false do
       indexes :title, analyzer: 'english', index_options: 'offsets'
+      indexes :title_suggest, type: 'completion', payloads: true
     end
   end
 
-  def as_indexed_json(options={})
-    self.as_json(
-      include: { genres: { only: :name},
-                 actors:    { only: :name },
-                 director:   { only: :name }
-               })
+  def as_indexed_json(_opts = {})
+    as_json(
+      include: {
+        genres:   { only: :name },
+        actors:   { only: :name },
+        director: { only: :name }
+      }
+    ).merge autocomplete_json
+  end
+
+  def autocomplete_json
+    {
+      title_suggest: {
+        input:  title,
+        output: title,
+        payload: { url: "/movies/#{id}" }
+      }
+    }
   end
 end
