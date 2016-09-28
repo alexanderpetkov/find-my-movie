8579fc54a54b1ae60be4f014bc576ef65167720e
Alexander Petkov <milchev.alexander@gmail.com>
2016-09-28 21:07:08 +0300

    Add movie_suggestions action & route

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,16 @@
 class SearchController < ApplicationController
-  before_action :sanitize, only: :movies
+  before_action :sanitize, only: [:movies, :movie_suggestions]
 
   def movies
     @movies = Movie.search(@query).records
+
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def movie_suggestions
+    @suggestions = Movie.suggest(@query)['movies'].first['options']
 
     respond_to do |format|
       format.js
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/' => 'public#welcome'
   get 'search/movies' => 'search#movies'
+  get 'search/movie_suggestions' => 'search#movie_suggestions'
 
   resources :movies, only: :show
 end
