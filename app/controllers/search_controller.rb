96a512c2c9acc00e78ff5439d77c298ff4259a92
Alexander Petkov <milchev.alexander@gmail.com>
2016-10-12 22:23:59 +0300

    Lint search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,11 @@
 class SearchController < ApplicationController
   before_action :parse_params, only: [:movies, :movie_suggestions]
 
-  MOVIE_RESULTS_COUNT = 10
+  MOVIE_COUNT = 10
 
   def movies
-    @movies = Movie.search(@query, size: MOVIE_RESULTS_COUNT, from: @from).records
-    @more_left = @movies.size >= MOVIE_RESULTS_COUNT
+    @movies = Movie.search(@query, size: MOVIE_COUNT, from: @from).records
+    @more_left = @movies.size >= MOVIE_COUNT
 
     respond_to do |format|
       format.js
