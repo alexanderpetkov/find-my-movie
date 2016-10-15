56ab721810734c9f103f72ec7ccf67a6d9f3fa99
Alexander Petkov <milchev.alexander@gmail.com>
2016-10-15 22:43:04 +0300

    Create movie search params specs

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -15,5 +15,47 @@ RSpec.describe SearchController, type: :controller do
         expect(assigns(:query)).to eq 'the Godfather 2'
       end
     end
+
+    context 'parsing offset' do
+      it 'defaults to 0 when not passed' do
+        get_xhr :movies, q: 'sth'
+
+        expect(assigns(:offset)).to be_zero
+      end
+
+      it 'is stored as fixnum' do
+        get_xhr :movies, q: 'sth', offset: 10
+
+        expect(assigns(:offset)).to be_kind_of Fixnum
+      end
+    end
+
+    context 'passing search params' do
+      it 'passes query to search' do
+        expect_args('sth', anything)
+
+        get_xhr :movies, q: 'sth', offset: 20
+      end
+
+      it 'wants 10 results' do
+        expect_args(anything, hash_including(size: 10))
+
+        get_xhr :movies, q: 'sth', offset: 20
+      end
+
+      it 'passes offset' do
+        expect_args(anything, hash_including(from: 20))
+
+        get_xhr :movies, q: 'sth', offset: 20
+      end
+
+      private
+
+      def expect_args(*expected)
+        expect(Movie).to receive(:search)
+          .with(*expected)
+          .and_return(double(records: {}))
+      end
+    end
   end
 end
