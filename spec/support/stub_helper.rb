0c66a1476e0a865db759d463b3b071e9021849c3
Alexander Petkov <milchev.alexander@gmail.com>
2016-10-18 19:35:17 +0300

    Make elasticsearch stub return content-type application/json
    To make sure JSONs objects are parsed when passing such :)

--- spec/support/stub_helper.rb
+++ spec/support/stub_helper.rb
@@ -9,7 +9,8 @@ module Support
                 'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
                 'User-Agent' => 'Faraday v0.9.2'
               })
-        .to_return(status: 200, body: '', headers: {})
+        .to_return(status: 200, body: '{}',
+                   headers: { 'Content-Type' => 'application/json' })
     end
   end
 end
